66c62b4c4dc6ff0486f2e261b23b884e2f0e3b38
N1chz <guldenbek0304@gmail.com>
2026-02-04 17:31:38 +0500

    Second Lab

--- Lab2/208.py
+++ Lab2/208.py
@@ -1,5 +1,13 @@
 n = int(input())
 b = 1
-for i in range(n):
-    b *= 2
-    print(b)
+if n == 1:
+        print("1", end=" ")
+else:
+        print(b, end=" ")
+        for i in range(n):
+            b *= 2
+            print(b, end=" ")
+            if b * 2 > n:
+                break
+            else:
+                continue
