3139fb830145d2092ac6c705526f971dadc93573
N1chz <guldenbek0304@gmail.com>
2026-02-05 19:21:06 +0500

    Second Lab

--- Lab2/209.py
+++ Lab2/209.py
@@ -1,13 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
-max = arr[0]
+
+min_val = min(arr)
+max_val = max(arr)
+
 for i in range(n):
-    if arr[i] > max:
-        max = arr[i]
-min = arr[0]
-for i in range(n):
-    if arr[i] < min:
-        min = arr[i]
-for i in range(n):
-    max = min
-    print(arr)
+    if arr[i] == max_val:
+        arr[i] = min_val
+
+print(' '.join(map(str, arr)))
