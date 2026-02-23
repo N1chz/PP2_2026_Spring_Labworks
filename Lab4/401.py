0df0e61c7bdd223484af9e81f6e17dc1bcea5743
N1chz <guldenbek0304@gmail.com>
2026-02-23 08:45:22 +0500

    N1chz

--- Lab4/401.py
+++ Lab4/401.py
@@ -1,3 +1,11 @@
-n = int(input())
-for i in range(n):
-    print((i + 1)**2)
+def sqrt_generator(n):
+    for i in range(1, n + 1):
+        yield i * i
+
+def main():
+    n = int(input().strip())
+    for sq in sqrt_generator(n):
+        print(sq)
+
+if __name__ == "__main__":
+    main()
