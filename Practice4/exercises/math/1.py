e8481a3f5a262f5195976e8021b751c19aa02030
N1chz <guldenbek0304@gmail.com>
2026-02-25 18:57:50 +0500

    N1chz

---
+++ Practice4/exercises/math/1.py
@@ -0,0 +1,6 @@
+import math
+
+degrees = float(input())
+radians = math.radians(degrees)
+
+print(f"{radians:.6f}")
---
+++ Practice4/exercises/math/3.py
@@ -0,0 +1,8 @@
+import math
+
+n = int(input())
+side = float(input())
+
+area = (n * side ** 2) / (4 * math.tan(math.pi / n))
+
+print(f"{area:.0f}")
